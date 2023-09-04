7be607e628f3317ae767c1d248df8c07a3340fbd
Jérôme Hénin <heninj@gmail.com>
2023-09-04 11:54:37 +0200

    Optimize by precomputing traj data

--- pyOptLE.py
+++ pyOptLE.py
@@ -39,7 +39,7 @@ def overdamped_Langevin(n, N, dt, beta = 1, D = .1, nablaV = nablaV_harm):
 
 def load_traj(filename):
     # TODO handle case of separate traj files
-    # Using colvars_traj module
+    # Using plot_colvar_traj module
 
     raw = np.loadtxt(filename)
     assert raw.shape[1] >= 2 and raw.shape[1] <= 3
@@ -77,64 +77,130 @@ def load_traj(filename):
 def history(res):
     history.opt.append(res)
     if history.nsteps%100 == 0:
-        print('Step', history.nsteps, np.square(res).sum())
+        print('Step %10i    Parameter mean sq. %10.2f' % (history.nsteps, np.mean(np.square(res))))
     history.nsteps += 1
 
 
-def optimize_model(initial_params, knots, q, deltaq, f, dt=1, T=300, RT=None):
-    # kcal/mol
-    R = 0.00198720425864
+def optimize_model(initial_params, knots, q, f=None, dt=1., T=300., RT=None):
+    '''Optimize an OptLE model given a trajectory.
+    This assumes Overdamped Langevin dynamics.
+    It uses an order-1 development of the short-term propagator.
+
+    Args:
+        initial_params (ndarray): initial parameters of the model
+        knots (ndarray): CV values forming the knots of the piecewise-linear approximation of logD and gradF.
+        q (list of ndarray): trajectories of the CV.
+        f (list of ndarray): trajectories of the biasing force.
+        dt (float, optional): time increment. Defaults to 1.
+        T (float, optional): Temperature (K). Defaults to 300.
+        RT (float, optional): RT. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    '''
+
+    obj = objective_order1_debiased
+
+    R = 0.00198720425864 # kcal/mol
     if RT is not None:
         RT = 1.
     else:
         RT = R * T
     beta = 1 / RT
 
+    print("Pre-processing trajectories...")
+    start_time = time.time()
+    traj = preprocess_traj(q, knots)
+    print("Done in %.3f seconds" % (time.time() - start_time))
+
+    if f is None:
+        f = [np.zeros_like(qi) for qi in q]
+    f = nb.typed.List(f)
+
+    print("Compiling and running objective function...")
+    start_time = time.time()
+    print('Obj = %.3f' % obj(initial_params, knots, traj, dt, beta, f)[0])
+    print("Compiled and ran in %.3f seconds" % (time.time() - start_time))
+
+    start_time = time.time()
+    print('Obj = %.3f' % obj(initial_params, knots, traj, dt, beta, f)[0])
+    print("Runs in %.3f seconds" % (time.time() - start_time))
+
+    # Record optimizer history
     history.opt = list()
     history.nsteps = 0
 
     start_time = time.time()
     # Minimize the objective function using L-BFGS-B algorithm
-    result = minimize(objective_order1_debiased, initial_params,  args=(knots, q, deltaq, dt, beta, f), jac=True, method='L-BFGS-B', callback=history)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    result = minimize(obj, initial_params,  args=(knots, traj, dt, beta, f), jac=True, method='L-BFGS-B', callback=history)
 
-    print(result.nit, result.message)
+    print(result.message)
+    print("Optimization converged in %i steps, %.3f seconds" % (result.nit, time.time() - start_time))
+
+    return result, history.opt
+
+
+def preprocess_traj(q, knots):
+    '''Preprocess colvar trajectory with a given grid for faster model optimization
+
+    Args:
+        q (list of ndarray): trajectories of the CV.
+        knots (ndarray): CV values forming the knots of the piecewise-linear approximation of logD and gradF.
+
+    Returns:
+        traj (numba types list): list of tuples (bin indices, bin positions, displacements)
+    '''
+    traj = list()
+
+    for qi in q:
+        deltaq = qi[1:] - qi[:-1]
+
+        # bin index on possibly irregular grid
+        # Truncate last traj point to match deltaq
+        idx = np.searchsorted(knots, qi[:-1])
+
+        assert (idx > 0).all() and (idx < len(knots)).all(), 'Out-of-bounds point(s) in trajectory\n'
+        # # Other option: fold back out-of-bounds points - introduces biases
+        # idx = np.where(idx == 0, 1, idx)
+        # idx = np.where(idx == len(knots), len(knots) - 1, idx)
+
+        q0, q1 = knots[idx - 1], knots[idx]
+        # fractional position within the bin
+        h = (qi[:-1] - q0) / (q1 - q0)
+
+        traj.append((idx, h, deltaq))
+
+    # Numba prefers typed lists
+    return nb.typed.List(traj)
+
 
 
 # WARNING - assumes fixed-size arrays - need to re-run cell each time data sizes change to recompile JIT
 # What happens once this is in a module?
 
-# TODO do search only once and pass indexes, h values
+@nb.njit
+def linear_interpolation_with_gradient(idx, h, knots, fp):
 
-@nb.njit(parallel=False)
-def linear_interpolation_with_gradient(x, xp, fp):
-    idx = np.searchsorted(xp, x)
-    # Assuming we are within bounds, or we get errors
-    # Faster without checks...
-    # idx = np.where(idx == 0, 1, idx)  # If idx == 0, set it to 1
-    # idx = np.where(idx == len(xp), len(xp) - 1, idx)  # If idx == len(xp), set it to len(xp) - 1
-    # assert (idx > 0).all() and (idx < len(xp)).all(), 'linear_interpolation_with_gradient: out of bounds \n' + str(x) + '\n' + str(xp)
-    n_knots = xp.shape[0]
-    x0, x1 = xp[idx - 1], xp[idx]
+    n_knots = knots.shape[0]
+    x0, x1 = knots[idx - 1], knots[idx]
     f0, f1 = fp[idx - 1], fp[idx]
-    # Second parameter set in second half of array
+    # Second parameter set is in second half of array
     g0, g1 = fp[idx - 1 + n_knots], fp[idx + n_knots]
 
-    hm = (x1 - x) / (x1 - x0)
-    hp = 1 - hm
-    val_f = hm * f0 + hp * f1
-    val_g = hm * g0 + hp * g1
+    hm = 1 - h
+    val_f = hm * f0 + h * f1
+    val_g = hm * g0 + h * g1
 
     # Set gradient elements one by one
-    grad = np.zeros((xp.shape[0], x.shape[0]))
+    grad = np.zeros((knots.shape[0], idx.shape[0]))
     for i, ik in enumerate(idx):
         grad[ik - 1, i] = hm[i]
-        grad[ik, i] = hp[i]        
+        grad[ik, i] = h[i]
     return val_f, val_g, grad
 
 
 @nb.njit(parallel=True)
-def objective_order1_debiased(params, knots, q, deltaq, dt, beta, f):
+def objective_order1_debiased(params, knots, traj, dt, beta, f):
     """Objective function: order-1 OptLE for overdamped Langevin, order-1 propagator
     Includes the debiasing feature of Hallegot, Pietrucci and Hénin for time-dependent biases
 
@@ -155,11 +221,13 @@ def objective_order1_debiased(params, knots, q, deltaq, dt, beta, f):
     dlogLdklD = np.zeros(dim)
     dlogLdkG = np.zeros(dim)
 
-    for i in nb.prange(len(q)):
-        G, logD, dXdk = linear_interpolation_with_gradient(q[i][:-1], knots, params)
+    # The loop over trajectories is parallelized by Numba
+    for i in nb.prange(len(traj)):
+        idx, h, deltaq = traj[i]
+        G, logD, dXdk = linear_interpolation_with_gradient(idx, h, knots, params)
         # dXdk is the gradient with respect to the knots (same for all quantities)
-        
-        # Debiasing
+
+        # Debiasing (truncate last traj point)
         G -= f[i][:-1]
     
         phi = - beta * np.exp(logD) * G * dt
@@ -168,10 +236,9 @@ def objective_order1_debiased(params, knots, q, deltaq, dt, beta, f):
         
         mu = 2.0 * np.exp(logD) * dt
         dmudlD = 2.0 * np.exp(logD) * dt
-
-        logL += (0.5 * logD + np.square(deltaq[i] - phi) / (2.0 * mu)).sum()
-        dLdlD = 0.5 + (2.0 * (deltaq[i] - phi) * -1.0 * dphidlD * (2.0 * mu) - np.square(deltaq[i] - phi) * 2.0 * dmudlD) / np.square(2.0 * mu)
-        dLdG = 2.0 * (deltaq[i] - phi) * -1.0 * dphidG / (2.0 * mu)
+        logL += (0.5 * logD + np.square(deltaq - phi) / (2.0 * mu)).sum()
+        dLdlD = 0.5 + (2.0 * (deltaq - phi) * -1.0 * dphidlD * (2.0 * mu) - np.square(deltaq - phi) * 2.0 * dmudlD) / np.square(2.0 * mu)
+        dLdG = 2.0 * (deltaq - phi) * -1.0 * dphidG / (2.0 * mu)
 
         dlogLdkG += np.dot(dXdk, dLdG)
         dlogLdklD += np.dot(dXdk, dLdlD)
@@ -205,14 +272,37 @@ def piecewise_linear_int(x, knots, optimized_G):
     return pred - np.min(np.array(pred))
 
 
-def finite_diff(x, dx, objective, **args):
+
+def check_gradient(obj, q, f, delta = 1e-6):
+    n_knots = 10
+    # Create grid and initial parameter set
+    epsilon = 1e-10
+    qmin_data = min([np.min(qi) for qi in q]) - epsilon
+    qmax_data = max([np.max(qi) for qi in q]) + epsilon
+    qmin = qmin_data
+    qmax = qmax_data
+    knots = np.linspace(qmin, qmax, n_knots)
+
+    # Initial guess for the parameters: zero free energy gradient, constant D = 1
+    initial_params = np.concatenate((np.zeros(n_knots), np.zeros(n_knots) + 1.))
+    traj = preprocess_traj(q, knots)
+
+    grad, g = finite_diff(initial_params, delta, objective_order1_debiased, knots, traj, 1, 1, f)
+
+    rms = np.sqrt(np.mean(np.square(grad)))
+    rmsd = np.sqrt(np.mean(np.square(grad - g)))
+
+    print('RMS gradient: %f     RMS difference: %f' % (rms, rmsd))
+
+
+def finite_diff(x, dx, objective, *args):
     
     grad = np.zeros_like(x)
-    y1, g = objective(x, **args)
+    y1, g = objective(x, *args)
 
     for i in range(x.shape[0]):
         x2 = np.copy(x)
         x2[i] += dx
-        y2, _ = objective(x2, **args)
+        y2, _ = objective(x2, *args)
         grad[i] = (y2-y1) / dx
     return grad, g
